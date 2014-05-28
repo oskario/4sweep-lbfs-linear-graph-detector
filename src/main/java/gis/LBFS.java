5425b4c09b26c1bf646eb19dd71e192fd6a255b7
Oskar <oskario2007@gmail.com>
2014-05-29 00:25:45 +0200

    LBFS templates added.

---
+++ src/main/java/gis/LBFS.java
@@ -0,0 +1,25 @@
+package gis;
+
+import java.util.List;
+
+/**
+ * Class that gathers all LBFS algorithms.
+ */
+public class LBFS {
+
+    public static List<Node> LBFS(Graph graph, Node first) {
+        return null;
+    }
+
+    public static List<Node> LBFSplus(Graph graph, List<Node> first) {
+        return null;
+    }
+
+    public static List<Node> LBFSplusplus(List<Node> first) {
+        return null;
+    }
+
+    public static boolean LBFSstar(List<Node> lbfs, List<Node> lbfsplus) {
+        return null;
+    }
+}
