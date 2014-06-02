aa9932fc925f85e5958a4dcbb102220ec895dfa7
Mupakoz <mupakoz@gmail.com>
2014-06-02 18:51:30 +0200

    Functional tests added

--- src/test/java/FunctionalTests.java
+++ src/test/java/FunctionalTests.java
@@ -20,21 +20,17 @@ public class FunctionalTests {
     }
 
     /**
-     * Tested set of intervals:
-     *
      * A  ---
      * B    ---
      * C      ---
      */
     @Test
     public void firstTest() throws IncorrectInputFormat {
-        String input = "A B\nB A\nC B";
+        String input = "A B\nB A C\nC B";
         assertTrue(isInterval(input));
     }
 
     /**
-     * Tested set of intervals:
-     *
      * A  ---
      * B    ---
      * C         ---
@@ -44,20 +40,81 @@ public class FunctionalTests {
     public void secondTest() throws IncorrectInputFormat {
         String input = "A B\nB A\nC D\nD C";
         Graph graph = Utils.readGraphFromString(input);
-        assertTrue(graph.isInterval(false));
+        assertTrue(!graph.isInterval(false));
     }
 
     /**
-     * Tested set of intervals:
-     *
      * A  ---
      * B    ---
-     * C         ---
-     * D           ---
+     * C  ---
      */
     @Test
     public void thirdTest() throws IncorrectInputFormat {
-        String input = "A B\nB A\nC D\nD C";
+        String input = "A B C\nB A C\nC A B";
+        assertTrue(isInterval(input));
+    }
+
+    /**
+     * A  ---
+     * B         ---
+     * C  ---
+     * D         ---
+     */
+    @Test
+    public void fourthTest() throws IncorrectInputFormat {
+        String input = "A C\nB D\nC A\nD B";
         assertTrue(!isInterval(input));
+    }
+
+    /**
+     * A  ---
+     * B   ----
+     * C      --------
+     * D        ---
+     * E            -----
+     */
+    @Test
+    public void fifthTest() throws IncorrectInputFormat {
+        String input = "A B\nB A C\nC B D E\nD C\nE D";
+        assertTrue(isInterval(input));
+    }
+
+    /**
+     * A  ---
+     * B   ----
+     * C      --------
+     * D            -----
+     * E                    ----
+     * F                     ----
+     */
+    @Test
+    public void sixthTest() throws IncorrectInputFormat {
+        String input = "A B\nB A C\nC B D\nD C\nE F\nF E";
+        assertTrue(!isInterval(input));
+    }
+
+    /**
+     * A  ---
+     * B   ----
+     * C      ----------
+     * D         ----
+     * E               ----
+     */
+    @Test
+    public void seventhTest() throws IncorrectInputFormat {
+        String input = "A B\nB A C\nC B D E\nD C\nE C";
+        assertTrue(isInterval(input));
+    }
+
+    /**
+     * A  -------------------
+     * B  ---
+     * C        ------
+     * D                 -------
+     */
+    @Test
+    public void eighthTest() throws IncorrectInputFormat {
+        String input = "A B C D\nB A\nC A\nD A";
+        assertTrue(isInterval(input));
     }
 }
